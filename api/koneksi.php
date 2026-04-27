9bfef67a63e94591421c8fa1a365df5649e4076a
ghandurfauzan-byt <ghandurfauzan@student.uns.ac.id>
2026-04-27 16:22:28 +0700

    Add files via upload

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,20 +1,11 @@
 <?php
-$hostname = 'gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com';
+$host = 'gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com';
 $port = 4000;
 $user = '2g8sUhwYN9NeyTE.root';
 $pass = 'eey7wFToimgjqrPT';
 $db   = 'db-pemweb';
 
-// Force IPv4
-$ip = gethostbyname($hostname);
-if ($ip && $ip !== $hostname) {
-    $host = $ip;
-} else {
-    $host = $hostname;
-}
-
 $koneksi = mysqli_init();
-// SSL tanpa CA verification
 mysqli_ssl_set($koneksi, NULL, NULL, NULL, NULL, NULL);
 $connected = mysqli_real_connect($koneksi, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL);
 
